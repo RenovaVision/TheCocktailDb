14860abbc459d1f162f387db6cbe856131a9ad99
AlexGolovach <alexgolovach1998@gmail.com>
2020-05-15 15:15:18 +0300

    Changed mapper

--- data/src/main/java/com/renovavision/thecocktaildb/data/repositories/CocktailsRepositoryImpl.kt
+++ data/src/main/java/com/renovavision/thecocktaildb/data/repositories/CocktailsRepositoryImpl.kt
@@ -1,7 +1,8 @@
 package com.renovavision.thecocktaildb.data.repositories
 
 import com.renovavision.thecocktaildb.data.api.CocktailsApi
-import com.renovavision.thecocktaildb.data.mapper.Mapper
+import com.renovavision.thecocktaildb.data.mapper.cocktailInfoToEntityMapper
+import com.renovavision.thecocktaildb.data.mapper.drinksToEntityMapper
 import com.renovavision.thecocktaildb.domain.entities.CocktailInfoEntity.CocktailEntity
 import com.renovavision.thecocktaildb.domain.entities.DrinksByQueryEntity.DrinkEntity
 import com.renovavision.thecocktaildb.domain.repositories.CocktailsRepository
@@ -12,14 +13,12 @@ import javax.inject.Singleton
 class CocktailsRepositoryImpl @Inject constructor(private val cocktailsApi: CocktailsApi) :
     CocktailsRepository {
 
-    private val mapper = Mapper()
-
     override suspend fun loadCocktailsListByIngredient(ingredient: String): List<DrinkEntity> =
-        cocktailsApi.loadDrinksByIngredient(ingredient).drinks.map { mapper.mapDrinkToEntity(it) }
+        cocktailsApi.loadDrinksByIngredient(ingredient).drinks.map { drinksToEntityMapper(it) }
 
     override suspend fun loadCocktailsListByCategory(category: String): List<DrinkEntity> =
-        cocktailsApi.loadDrinksByCategory(category).drinks.map { mapper.mapDrinkToEntity(it) }
+        cocktailsApi.loadDrinksByCategory(category).drinks.map { drinksToEntityMapper(it) }
 
     override suspend fun loadCocktailDetails(id: Int): List<CocktailEntity> =
-        cocktailsApi.loadCocktailInfoById(id).drinks.map { mapper.mapCocktailToEntity(it) }
+        cocktailsApi.loadCocktailInfoById(id).drinks.map { cocktailInfoToEntityMapper(it) }
 }
--- data/src/main/java/com/renovavision/thecocktaildb/data/repositories/SearchRepositoryImpl.kt
+++ data/src/main/java/com/renovavision/thecocktaildb/data/repositories/SearchRepositoryImpl.kt
@@ -1,7 +1,7 @@
 package com.renovavision.thecocktaildb.data.repositories
 
 import com.renovavision.thecocktaildb.data.api.CocktailsApi
-import com.renovavision.thecocktaildb.data.mapper.Mapper
+import com.renovavision.thecocktaildb.data.mapper.cocktailInfoToEntityMapper
 import com.renovavision.thecocktaildb.domain.repositories.SearchRepository
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,8 +10,6 @@ import javax.inject.Singleton
 class SearchRepositoryImpl @Inject constructor(private val cocktailsApi: CocktailsApi) :
     SearchRepository {
 
-    private val mapper = Mapper()
-
     override suspend fun loadSearchResult(query: String) =
-        cocktailsApi.searchCocktails(query).drinks.map { mapper.mapCocktailToEntity(it) }
+        cocktailsApi.searchCocktails(query).drinks.map { cocktailInfoToEntityMapper(it) }
 }
